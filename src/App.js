9328cd6455e74fea45b45c2db3a76d86467660bc
Guilherme Paiva <guilhermepaiva@MacBook-Pro-de-Guilherme.local>
2021-12-28 11:19:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Movies from './components/Movies';
+import Home from './components/Home';
+import Admin from './components/Admin';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+
+        <div className="row">
+          <h1 className='mt-3'>Go Watch a Movie</h1>
+          <hr className='mb-3'></hr>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-2'>
+            <nav>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="admin">Manage Catalog</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className='col-md-10'>
+            <Routes>
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+    return (
+        <div>
+            <h1>Movies Catalog here</h1>
+        </div>
+    )
+}
+
+export default Admin
